4d1f0977614a11ef5b0a10705f52689588e3023c
Julia <juzicastro12@gmail.com>
2021-06-21 18:56:50 -0300

    função apagar e categorias

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import ListaDeCategorias from "./components/ListaDeCategorias/ListaDeCategorias";
 import "./assets/App.css";
 import './assets/index.css';
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 
 class App extends Component {
   
-  constructor(){
+  constructor() {
     super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
     
-    //Propriedade especial. Toda vez que é atualizada, o react chama o método render;
-    this.state = {
-      notas:[]
-    }
-  }
-
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
-    const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
   }
 
   render() {
     return (
       <section className="conteudo">
-        {/*Da pra passar propriedades pros filhos como em criarNota( em formulario) que passa referência pro elemento pai
-        entao qdo eu chamo o criarNota é possivel mudar dinamicante os dados (injeção de dependecia)*/}
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/> 
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro
+          categorias={this.categorias}
+          criarNota={this.notas.adicionarNota.bind(this.notas)}
+        />
+        <main className="conteudo-principal">
+          <ListaDeCategorias
+            adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+            categorias={this.categorias}
+          />
+          <ListaDeNotas
+            apagarNota={this.notas.apagarNota.bind(this.notas)}
+            notas={this.notas}
+          />
+        </main>
       </section>
     );
   }
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,16 +3,37 @@ import CardNota from "../CardNota/CardNota";
 import "./style.css";
 
 class ListaDeNotas extends Component {
-  // Não preciso do construtor porém as propriedas são passadas.
+
+  constructor(){
+    super();
+    this.state = {notas:[]}
+    this._novasNotas = this._novasNotas.bind(this);
+  }
+  componentDidMount(){
+    this.props.notas.inscrever(this._novasNotas);
+  }
+  componentWillUnmount(){
+    this.props.notas.desinscrever(this._novasNotas);
+  }
+  _novasNotas(notas){
+    this.setState({...this.state,notas})
+  }
 
   render() {
+
     return (
       <ul className="lista-notas">
-        {/* array de notas */}
-        {this.props.notas.map((nota, index) => {
+        {this.state.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+
+              <CardNota
+              indice={index}
+              apagarNota={this.props.apagarNota}
+              titulo={nota.titulo}
+              texto={nota.texto}
+              categoria={nota.categoria}
+              />
             </li>
           );
         })}
